2df9278a90639d23425037a1ad5b2414398cf2c7
AntonioDo2001 <Antonio.Dominguez5@alu.uclm.es>
2025-09-20 12:18:10 +0200

    Preparación BD en PostgreSql y actualización requirements.txt

--- zooFANDIT/zooFANDIT/settings.py
+++ zooFANDIT/zooFANDIT/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crudZoo',
     'rest_framework',
+    'crudZoo',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'zooFANDIT.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'fandit_zoo',
+        'USER': 'zoo_user',
+        'PASSWORD': 'zoo_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
